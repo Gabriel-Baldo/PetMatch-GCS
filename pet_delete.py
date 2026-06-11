e65fcf3b6a2b8bbfa4dc4fa4ee5dfe70580f7483
Renan Pilonetto <rgabrielpilonetto@gmail.com>
2026-06-10 21:22:32 -0300

    feat: adiciona script para deletar pets em py

---
+++ pet_delete.py
@@ -0,0 +1,12 @@
+def excluir_pet(lista_pets):
+    print("--- Exclusão de Pet ---")
+
+    nome = input("Nome do Pet a ser excluído: ")
+
+    for pet in lista_pets:
+        if pet.nome == nome:
+            lista_pets.remove(pet)
+            print(f"Sucesso! {nome} foi excluído do sistema!")
+            return
+
+    print("Pet não encontrado.")g
