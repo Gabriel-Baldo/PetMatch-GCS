b0599a1fc51202108fcab04a32cbd64b96f4c4dc
Lucas Bueno <lucasbuenodossantos2023@gmail.com>
2026-06-12 14:45:04 -0300

    feat:adiciona script de busca e listagem de usuarios em py

--- user_crud.py
+++ user_crud.py
@@ -11,3 +11,20 @@ def criar_usuario(nome, email, telefone):
     usuarios.append(usuario)
     id_atual += 1
     print("Usuário criado com sucesso!")
+
+def listar_usuarios():
+    if not usuarios:
+        print("Nenhum usuário cadastrado.")
+        return
+
+    for u in usuarios:
+        print(u)
+
+
+def buscar_usuario(id_usuario):
+    for u in usuarios:
+        if u["id"] == id_usuario:
+            print(u)
+            return
+    print("Usuário não encontrado")
+
